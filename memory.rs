8c60a7cfd1d25b6c33eab98514ac9d4112e8a524
Daniel Micay <danielmicay@gmail.com>
2014-06-18 16:21:06 -0400

    update to the fixed `mmap` family signatures

--- memory.rs
+++ memory.rs
@@ -1,4 +1,4 @@
-use core::ptr::{mut_null, null};
+use core::ptr::mut_null;
 use libc::{PROT_READ, PROT_WRITE, MAP_ANON, MAP_PRIVATE, MAP_FAILED, c_int, c_void, mmap, munmap,
            size_t};
 
@@ -10,7 +10,8 @@ extern {
 static MREMAP_MAYMOVE: c_int = 1;
 
 pub unsafe fn map(size: uint) -> *mut u8 {
-    let ptr = mmap(null(), size as size_t, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    let ptr = mmap(mut_null(), size as size_t, PROT_READ | PROT_WRITE,
+                   MAP_ANON | MAP_PRIVATE, -1, 0);
     if unlikely!(ptr == MAP_FAILED as *mut c_void) { return mut_null() }
     ptr as *mut u8
 }
@@ -27,5 +28,5 @@ pub unsafe fn remap_inplace(ptr: *mut u8, old_size: uint, new_size: uint) -> boo
 }
 
 pub unsafe fn unmap(ptr: *mut u8, size: uint) {
-    munmap(ptr as *c_void, size as size_t);
+    munmap(ptr as *mut c_void, size as size_t);
 }
